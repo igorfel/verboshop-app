f415d929d211e0b4cfb274d439971574dc0739cc
igorfel <appsigor@gmail.com>
2018-07-03 14:41:41 -0300

    #13 Lista configurada para exibir dados do banco

--- lib/screens/Home/index.dart
+++ lib/screens/Home/index.dart
@@ -5,7 +5,6 @@ import "style.dart";
 import 'package:verboshop/services/authentication.dart';
 import 'package:verboshop/services/audiosManager.dart';
 
-import 'package:local_notifications/local_notifications.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key key }) : super(key: key);
@@ -18,7 +17,6 @@ class HomeScreenState extends State<HomeScreen>{
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
-  bool showPlayIcon = true;
   // int currentPlaying = -1, lastPlayingIndex = -1;
   // Duration lastDuration, lastPosition;
   // String currentTitle, currentContent;
@@ -37,7 +35,7 @@ class HomeScreenState extends State<HomeScreen>{
     userAuth.signOut().then((onValue) {
       if(onValue == "Logout Successfull"){
         audiosManager.stop();
-        _cancelNotification();
+        //_cancelNotification();
         Navigator.pushNamedAndRemoveUntil(context, "/Login", (_) => false);
       } else {
         showInSnackBar(onValue);
@@ -47,66 +45,8 @@ class HomeScreenState extends State<HomeScreen>{
     });
   }
 
-  void _playAudio(int index) async {
-    await audiosManager.play(index);
-
-    _showNotification(audiosManager.listOfAudios[index].title, "Ministro: " + audiosManager.listOfAudios[index].minister);
-
-    updateUI();
-  }
-
-  void _pauseAudio() async {
-    await audiosManager.pause();
-
-    updateUI();
-  }
-
-  void playPauseOnNotification(String payload) async {
-    if(audiosManager.isPlaying) {
-      await audiosManager.pause();
-    } else {
-      await audiosManager.resume();
-    }
-
-    updateUI();
-  }
-
-  void updateUI() {
-    if(audiosManager.isPlaying){
-      setState(() { showPlayIcon = false; });
-    } else {
-      setState(() { showPlayIcon = true; });
-    }
-  }
-
-  Future _showNotification(String title, String content) async {
-    await LocalNotifications.createNotification(
-        title: title,
-        content: content,
-        id: 0,
-        actions: [
-            new NotificationAction(
-                actionText: "Pausar/Reproduzir",
-                callback: playPauseOnNotification,
-                payload: "",
-                launchesApp: false
-            )
-        ]
-    );
-  }
-
-  Future _cancelNotification() async {
-    await LocalNotifications.removeNotification(0);
-  }
-
   @override
   Widget build(BuildContext context) {
-
-    audiosManager.audioPlayer.setCompletionHandler(() {
-      audiosManager.complete();
-
-      setState(() { showPlayIcon = true; });
-    });
 
     // TODO: implement build
     return new Scaffold(
@@ -121,32 +61,34 @@ class HomeScreenState extends State<HomeScreen>{
               ),
             ],
           ),
-      body:new Center(
-        child:
-          new ListView.builder(
-            padding: new EdgeInsets.all(8.0),
-            itemCount: audiosManager.listOfAudios.length,
-            itemExtent: 80.0,
-            itemBuilder: (BuildContext context, int index) {
-              return new ListTile(
-                //leading: const Icon(Icons.flight_land),
-                title: new Row(
-                  children: <Widget>[
-                    new Expanded(child: new Text(audiosManager.listOfAudios[index].title)),
-                    new IconButton(
-                      icon: (audiosManager.currentPlaying == index && !showPlayIcon) ? new Icon(Icons.pause) : new Icon(Icons.play_arrow),
-                      tooltip: 'Ouvir/pausar ministração',
-                      onPressed: (audiosManager.currentPlaying == index && !showPlayIcon) ? () => _pauseAudio() : () => _playAudio(index),
-
-                    ),
-                  ],
-                ),
-                subtitle: Text('Ministro: ' + audiosManager.listOfAudios[index].minister),
-                //onTap: () { /* react to the tile being tapped */ }
-              );
-            },
-          )
-      )
+      body: new AudioList(audiosManager),
     );
   }
 }
+
+// new Center(
+//         child:
+//           new ListView.builder(
+//             padding: new EdgeInsets.all(8.0),
+//             itemCount: audiosManager.listOfAudios.length,
+//             itemExtent: 80.0,
+//             itemBuilder: (BuildContext context, int index) {
+//               return new ListTile(
+//                 //leading: const Icon(Icons.flight_land),
+//                 title: new Row(
+//                   children: <Widget>[
+//                     new Expanded(child: new Text(audiosManager.listOfAudios[index].title)),
+//                     new IconButton(
+//                       icon: (audiosManager.currentPlaying == index && !showPlayIcon) ? new Icon(Icons.pause) : new Icon(Icons.play_arrow),
+//                       tooltip: 'Ouvir/pausar ministração',
+//                       onPressed: (audiosManager.currentPlaying == index && !showPlayIcon) ? () => _pauseAudio() : () => _playAudio(index),
+
+//                     ),
+//                   ],
+//                 ),
+//                 subtitle: Text('Ministro: ' + audiosManager.listOfAudios[index].minister),
+//                 //onTap: () { /* react to the tile being tapped */ }
+//               );
+//             },
+//           )
+//  )
--- lib/services/audiosManager.dart
+++ lib/services/audiosManager.dart
@@ -2,48 +2,34 @@ import 'package:verboshop/models/audio.dart';
 import 'package:audioplayers/audioplayer.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:local_notifications/local_notifications.dart';
+
 class AudiosManager {
   List<Audio> listOfAudios = new List();
+  List<DocumentSnapshot> listOfAudiosCloud;
 
   bool isPlaying = false;
-  int currentPlaying = -1, lastPlayingIndex = -1;
+  String currentPlaying = "", lastPlayingIndex = "";
   Duration lastDuration, lastPosition;
   String currentTitle, currentContent;
 
   AudioPlayer audioPlayer = new AudioPlayer();
-  
-  AudiosManager() {
-    retrieveAudioList();
-  }
-  
-  /**
-   * Access the database to retrieve audio information
-   */
-  void retrieveAudioList() {
-    listOfAudios.add(new Audio(title: "Título 1", minister: "Ministro 1", date: "10/10/2017", url: "http://www.rxlabz.com/labz/audio2.mp3"));
-    listOfAudios.add(new Audio(title: "Título 2", minister: "Ministro 3", date: "10/10/2017", url: "http://www.rxlabz.com/labz/audio.mp3"));
-    listOfAudios.add(new Audio(title: "Realidades da nova criação", minister: "Pr. Jaelson", date: "25/03/2018", url: "https://vocaroo.com/media_command.php?media=s17dSeJPekw6&command=download_mp3"));
-    listOfAudios.add(new Audio(title: "Escatologia - Parte 1", minister: "Pr. Antonio Teste", date: "25/03/2018", url: "http://www.rxlabz.com/labz/audio.mp3"));
-    listOfAudios.add(new Audio(title: "Família um projeto de Deus", minister: "Adriane Oliveira Teste", date: "25/03/2018", url: "http://www.rxlabz.com/labz/audio.mp3"));
 
-    //await new Future.delayed(new Duration(seconds: 3));
-
-    //return null;
-  }
-
-  Future play(int index) async {
-    if(isPlaying && currentPlaying == index) return;
+  Future play(String url) async {
+    if(isPlaying && currentPlaying == url) return;
     
-    if(isPlaying || index != lastPlayingIndex)
+    if(isPlaying || url != lastPlayingIndex)
       audioPlayer.stop();
 
-    final result = await audioPlayer.play(listOfAudios[index].url, isLocal: false);
+    final result = await audioPlayer.play(url, isLocal: false);
 
     if(result == 1) {
       isPlaying = true;
-      currentPlaying = index;
+      currentPlaying = url;
 
-      print("Played: " + listOfAudios[currentPlaying].title);
+      print("Played: " + currentPlaying);
     }
   }
 
@@ -55,9 +41,9 @@ class AudiosManager {
     if(result == 1) {
       isPlaying = false;
       lastPlayingIndex = currentPlaying;
-      currentPlaying = -1;
+      currentPlaying = "";
 
-      print("Paused: " + listOfAudios[lastPlayingIndex].title);
+      print("Paused: " + lastPlayingIndex);
     }
   }
   
@@ -68,14 +54,133 @@ class AudiosManager {
   void stop() {
     audioPlayer.stop();
     isPlaying = false;
-    currentPlaying = -1;
+    currentPlaying = "";
   }
 
   void complete() {
     isPlaying = false;
     lastPlayingIndex = currentPlaying;
-    currentPlaying = -1;
+    currentPlaying = "";
 
-    print("Completed: " + listOfAudios[lastPlayingIndex].title);
+    print("Completed: " + currentPlaying);
+  }
+}
+
+class AudioList extends StatefulWidget {
+  AudiosManager audiosManager;
+
+  AudioList(AudiosManager audiosManager){
+    this.audiosManager = audiosManager;
+  }  
+
+  @override
+  AudiosListState createState() => new AudiosListState(audiosManager);
+}
+
+class AudiosListState extends State<AudioList> {
+  AudiosManager audiosManager;
+  bool showPlayIcon = true;
+
+  AudiosListState(AudiosManager audiosManager) {
+    this.audiosManager = audiosManager;
+  }
+
+  void _playAudio(DocumentSnapshot document) async {
+    await audiosManager.play(document['url']);
+
+    _showNotification(document['title'], "Ministro: " + document['author']);
+
+    updateUI();
+  }
+
+  static const AndroidNotificationChannel channel = const AndroidNotificationChannel(
+    id: '0',
+    name: 'CustomNotificationChannel',
+    description: 'Grant this app the ability to show notifications',
+    importance: AndroidNotificationChannelImportance.LOW,
+    vibratePattern: AndroidVibratePatterns.NONE
+  );
+
+  Future _showNotification(String title, String content) async {
+    await LocalNotifications.createNotification(
+        title: title,
+        content: content,
+        id: 0,
+        actions: [
+            new NotificationAction(
+                actionText: "Pausar/Reproduzir",
+                callback: playPauseOnNotification,
+                payload: "",
+                launchesApp: false
+            )
+        ],
+        androidSettings: new AndroidSettings(
+          channel: channel,
+          priority: AndroidNotificationPriority.LOW
+        )
+    );
+  }
+
+  Future _cancelNotification() async {
+    await LocalNotifications.removeNotification(0);
+  }
+
+  void _pauseAudio() async {
+    await audiosManager.pause();
+
+    updateUI();
+  }
+
+  void playPauseOnNotification(String payload) async {
+    if(audiosManager.isPlaying) {
+      await audiosManager.pause();
+    } else {
+      await audiosManager.resume();
+    }
+
+    updateUI();
+  }
+
+  void updateUI() {
+    if(audiosManager.isPlaying){
+      setState(() { showPlayIcon = false; });
+    } else {
+      setState(() { showPlayIcon = true; });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    audiosManager.audioPlayer.setCompletionHandler(() {
+      audiosManager.complete();
+      setState(() { showPlayIcon = true; });
+    });
+
+    return new StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('audios').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) return new Text('Loading...');
+        return new ListView(
+          padding: new EdgeInsets.all(8.0),
+          itemExtent: 80.0,
+          children: snapshot.data.documents.map((DocumentSnapshot document) {
+            return new ListTile(
+              title: new Row(
+                children: <Widget>[
+                  new Expanded(child: new Text(document['title'])),
+                  new IconButton(
+                    icon: (audiosManager.currentPlaying == document['url'] && !showPlayIcon) ? new Icon(Icons.pause) : new Icon(Icons.play_arrow),
+                    tooltip: 'Ouvir/pausar ministração',
+                    onPressed: (audiosManager.currentPlaying == document['url'] && !showPlayIcon) ? () => _pauseAudio() : () => _playAudio(document),
+
+                  ),
+                ],
+              ),
+              subtitle: new Text("Ministro: " + document['author']),
+            );
+          }).toList(),
+        );
+      },
+    );
   }
 }
